fcd4a9fa1943f2a8dd93844189c5413b18f476ba
dewana_kl <53048107+dewanakl@users.noreply.github.com>
2021-10-15 15:58:04 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// folder to save files (please rename random folder)
+define('dirSave', 'file' . '/');
+
+// folder size (bytes)
+define('folderSize', 524288000);
+
+// max upload file size (bytes) *adjust the settings in php
+define('maxfileSize', 52428800);
+
+// define file .txt
+define('paste', 'paste.txt');
